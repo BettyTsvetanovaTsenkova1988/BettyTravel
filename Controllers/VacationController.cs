b6777ec571da1987c190a2b6727c447f4ceaeb83
BettyTsvetanovaTsenkova1988 <bety_1988@abv.bg>
2017-05-10 23:17:30 +0300

    Add files via upload

---
+++ Controllers/VacationController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BettyTravel.Services;
+using BettyTravelApp.Models.EntityModels;
+using BettyTravelApp.Models.ViewModels.Vacation;
+
+namespace BettyTravelApp.Controllers
+{
+    [RoutePrefix("vacations")]
+    [AllowAnonymous]
+    public class VacationController : Controller
+    {
+        private VacationService service;
+
+        public VacationController()
+        {
+            this.service = new VacationService();
+        }
+
+
+        [HttpGet, Route("details/{id}")]
+        public ActionResult Details(int id)
+        {
+            if (id <= 0)
+            {
+                return this.View("VacationNotFound");
+            }
+
+            VacationDetailsViewModel vm = this.service.GetDetails(id);
+            if (vm == null)
+            {
+                return this.View("VacationNotFound");
+            }
+
+            return this.View(vm);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin, customer")]
+        [Route("buyVacation")]
+        public ActionResult BuyVacation(int vacationId)
+        {
+            try
+            {
+                string customerName = this.User.Identity.Name;
+                Customer customer = this.service.GetCurrentCustomer(customerName);
+                this.service.BuyVacation(vacationId, customer);
+                return this.RedirectToAction("Profile", "Customers");
+            }
+            catch (Exception)
+            {
+
+                return this.View("Error");
+            }
+
+
+        }
+
+
+    }
+}
